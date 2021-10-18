892d43f84788739aae72440dd486b1a4dfc49001
Alan Sanchez <realalansan@gmail.com>
2021-10-18 14:22:24 -0700

    Fix unneeded import

--- pages/dogs.tsx
+++ pages/dogs.tsx
@@ -1,23 +1,14 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import animals from "../animals.json";
 
 const Animal: NextPage = () => {
   return (
     <div>
       <h1>Animals</h1>
 
-      <ul>
-        {Object.entries(animals["dogs"]).map((value, index) => {
-          return (
-            <li key={index}>
-              <Link href="/animal/[id]" as={"/animal/" + value[0]}>
-                <a>{value[1].name}</a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <Link href="/dogs">
+        <a>Dogs</a>
+      </Link>
     </div>
   );
 };
