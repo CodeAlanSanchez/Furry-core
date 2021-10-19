33d4ac08cca7ac4c22980f07a038d6141d7b9027
Alan Sanchez <realalansan@gmail.com>
2021-10-19 16:08:05 -0700

    Add onClick event handler to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "../styles/Button.module.css";
 
-const Button = ({ children }: any) => {
-  return <button className={styles.button}>{children}</button>;
+const Button = ({ children, onClick }: any) => {
+  return (
+    <button onClick={onClick} className={styles.button}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
