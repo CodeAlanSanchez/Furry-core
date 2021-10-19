4e0224d5f117dba6f4cb42a1b2d90ad29cb7248e
Alan Sanchez <realalansan@gmail.com>
2021-10-19 16:05:55 -0700

    Add basic layout to dog page

--- pages/dogs.tsx
+++ pages/dogs.tsx
@@ -1,14 +1,29 @@
 import type { NextPage } from "next";
-import Link from "next/link";
+import Image from "next/image";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDogs } from "../api";
+import Button from "../components/Button";
+import styles from "../styles/Dog.module.css";
 
 const Animal: NextPage = () => {
-  return (
-    <div>
-      <h1>Animals</h1>
+  const dispatch = useDispatch();
+  const dogs = useSelector((state: { dogs: [{}] }) => state.dogs);
 
-      <Link href="/dogs">
-        <a>Dogs</a>
-      </Link>
+  // useEffect(() => {
+  //   dispatch(fetchDogs);
+  // }, []);
+
+  return (
+    <div className={styles.content}>
+      <Image
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Qe-7V3Hiec0ZVD2f31FFzQHaEK%26pid%3DApi&f=1"
+        alt="Dog"
+        width={800}
+        height={450}
+        className={styles.image}
+      />
+      <Button>New Dog</Button>
     </div>
   );
 };
