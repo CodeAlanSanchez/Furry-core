cd57d6135f9f7d68f47344febf5e06ecf61767c0
Alan Sanchez <realalansan@gmail.com>
2021-10-21 05:09:37 -0700

    Update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,19 @@ import styles from "../styles/Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
+      <Link href="/cats">
+        <a>
+          <h6>Cats</h6>
+        </a>
+      </Link>
       <Link href="/">
         <a>
-          <h3>Furry Core</h3>
+          <h3 className={styles.logo}>Furry Core</h3>
+        </a>
+      </Link>
+      <Link href="/dogs">
+        <a>
+          <h6>Dogs</h6>
         </a>
       </Link>
     </nav>
