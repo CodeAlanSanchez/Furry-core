f6b0d3351d48e5205d3cb517e9a7b99e278b2e24
Alan Sanchez <realalansan@gmail.com>
2021-10-21 05:35:56 -0700

    Add cats to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,21 @@ const Home: NextPage = () => {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.dogs}>
+        <div className={styles.animals}>
+          <h4>Cats</h4>
+          <Link href="/dogs">
+            <a className={styles.imageContainer}>
+              <Image
+                src="https://cataas.com/cat/616de562fd416c0017325d2a"
+                className={styles.image}
+                layout="fill"
+                objectFit="contain"
+                alt="dogs"
+              />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.animals}>
           <h4>Dogs</h4>
           <Link href="/dogs">
             <a className={styles.imageContainer}>
