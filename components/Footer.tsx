57fbec2f27f129e0072430774d6415f81286b7aa
Alan Sanchez <realalansan@gmail.com>
2021-11-21 19:50:11 -0800

    Add empty footer back to preserve whitespace

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.content}>
+      {/* <div className={styles.content}>
         <h4>Like this page? Consider donating 🐶</h4>
         <div className={styles.donate}>
           <p className={[styles.bitcoin, styles.info].join(" ")}>
@@ -15,7 +15,7 @@ const Footer = () => {
             Ethereum address: 0xAe51562b8fe1aE7bA13Ed8497CEdfEbC46Ffe5F3
           </p>
         </div>
-      </div>
+      </div> */}
     </footer>
   );
 };
